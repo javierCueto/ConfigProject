ea342fa3c599a53fd3904aa72d6c8887447f5900
Javier Cueto <39851501+javierCueto@users.noreply.github.com>
2022-10-19 12:58:12 -0600

    Initial Commit

---
+++ ConfigProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ConfigProject
+//
+//  Created by Javier Cueto on 19/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
